b3464366bd67ba4dd19905246bf6d7f91da1c05e
PaivaNeto <netopaivacontador@gmail.com>
2024-09-16 19:12:09 -0300

    Aula 02

---
+++ Aula0102/Atividades.py
@@ -0,0 +1,44 @@
+# QUESTÃO 01)
+
+# idade1 = input ("Qual a idade 1?")
+# idade2 = input ("Qual a idade 2?")
+
+# if idade1 > idade2:
+#     print("A idade 1 é menor que a idade 2")
+# elif idade1 < idade2:
+#     print("A idade 1 é maior que a idade 2")
+# else:
+#     print("As idades são iguais.")
+
+# QUESTÃO 02)
+# palavra1 = input ("Digite uma palavra!")
+# palavra2 = input ("Digite uma palavra!")
+
+# if palavra1==palavra2:
+#     print("As palavras são iguais!")
+# else: 
+#     print("As palavras são diferentes")
+
+
+#if palavra1==palavra2:
+#    print("As palavras são iguais!")
+#elif palavra1!=palavra2:
+#    print("As palavras são diferentes!")
+
+#QUESTÃO 03)
+
+idade = int(input("Qual sua idade?"))
+habilitado = input ("Habilitado?")
+
+if idade >= 18 and habilitado == "sim":
+    print(f"Sua idade é de {idade} e você já possui CNH")
+elif idade >=18 and habilitado == "não":
+    print(f"Sua idade é de {idade} e você não possui CNH")
+else:
+    print(f"Sua idade é {idade} e vc não pode ter CNH")
+
+
+
+
+
+
