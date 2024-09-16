1cddb4aec8016373e595c600e7b66abd975eaf9b
PaivaNeto <netopaivacontador@gmail.com>
2024-09-16 19:13:24 -0300

    Update Atividades.py
    Atividades realizadas em sala

--- Aula0102/Atividades.py
+++ Aula0102/Atividades.py
@@ -1,29 +1,29 @@
 # QUESTÃO 01)
 
-# idade1 = input ("Qual a idade 1?")
-# idade2 = input ("Qual a idade 2?")
+idade1 = input ("Qual a idade 1?")
+idade2 = input ("Qual a idade 2?")
 
-# if idade1 > idade2:
-#     print("A idade 1 é menor que a idade 2")
-# elif idade1 < idade2:
-#     print("A idade 1 é maior que a idade 2")
-# else:
-#     print("As idades são iguais.")
+ if idade1 > idade2:
+     print("A idade 1 é menor que a idade 2")
+ elif idade1 < idade2:
+     print("A idade 1 é maior que a idade 2")
+else:
+    print("As idades são iguais.")
 
 # QUESTÃO 02)
-# palavra1 = input ("Digite uma palavra!")
-# palavra2 = input ("Digite uma palavra!")
+ palavra1 = input ("Digite uma palavra!")
+ palavra2 = input ("Digite uma palavra!")
 
-# if palavra1==palavra2:
-#     print("As palavras são iguais!")
-# else: 
-#     print("As palavras são diferentes")
+ if palavra1==palavra2:
+     print("As palavras são iguais!")
+ else: 
+     print("As palavras são diferentes")
 
 
-#if palavra1==palavra2:
-#    print("As palavras são iguais!")
-#elif palavra1!=palavra2:
-#    print("As palavras são diferentes!")
+if palavra1==palavra2:
+    print("As palavras são iguais!")
+elif palavra1!=palavra2:
+   print("As palavras são diferentes!")
 
 #QUESTÃO 03)
 
